343d7e22adfb89945faa81ad7d63e9661dd8e480
atec <root@atec.pub>
2018-10-06 18:05:55 -0400

    log hits to CloudKit

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"crypto"
 	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"log/syslog"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -23,9 +31,11 @@ const (
 )
 
 var (
-	logger *syslog.Writer
-
 	etc = os.Getenv("ETC")
+
+	client http.Client
+
+	path = "/database/1/iCloud.telos.atec/development/public/records/modify"
 
 	kids = map[string]string{
 		"music": "CUG44HA5T5",
@@ -34,29 +44,31 @@ var (
 	keys = make(map[string]*ecdsa.PrivateKey)
 )
 
-func checkSyslog(err error) {
-	if err != nil {
-		fmt.Println("failed to write to syslog", err)
-	}
-}
-
 func init() {
 
 	var err error
 
-	logger, err = syslog.Dial("tcp", "35.231.223.50:514", syslog.LOG_INFO, "auth")
+	b, err := ioutil.ReadFile(etc + "cloudkit/eckey.pem")
 	if err != nil {
-		log.Fatal("dialing syslog", err)
+		log.Fatal("reading cloudkit pem:", err)
 	}
+	block, _ := pem.Decode(b)
+
+	priv, err := x509.ParseECPrivateKey(block.Bytes)
+	if err != nil {
+		log.Fatal("parsing cloudkit ec key:", err)
+	}
+
+	keys["cloud"] = priv
 
 	for svc, kid := range kids {
 
 		path := etc + "/" + svc + cat + kid + ext
-		checkSyslog(logger.Info("loading auth key from path: " + path))
+		fmt.Println("loading auth key from path: " + path)
 
 		key, err := token.AuthKeyFromFile(path)
 		if err != nil {
-			checkSyslog(logger.Err("reading p8 file: " + err.Error()))
+			fmt.Println("reading p8 file: " + err.Error())
 		}
 
 		keys[svc] = key
@@ -65,7 +77,54 @@ func init() {
 
 func middleware(w http.ResponseWriter, r *http.Request) bool {
 
-	checkSyslog(logger.Info(r.Method + " " + r.URL.Path + " " + r.RemoteAddr + " " + r.Host))
+	fmt.Println(r.Method + " " + r.URL.Path + " " + r.RemoteAddr + " " + r.Host)
+
+	payload := []byte(`
+{
+	"operations": [
+		{
+			"operationType": "create",
+			"record": {
+				"recordType": "Hit",
+				"fields": {
+					"method": { "value": "` + r.Method + `" },
+					"path": { "value": "` + r.URL.Path + `" },
+					"remoteAddr": { "value": "` + r.RemoteAddr + `" },
+					"host": { "value": "` + r.Host + `" }
+				}
+			}
+		}
+	]
+}`)
+
+	date := time.Now().UTC().Format(time.RFC3339)
+
+	h := sha256.New()
+	h.Write(payload)
+	body := base64.StdEncoding.EncodeToString(h.Sum(nil))
+
+	msg := strings.Join([]string{date, body, path}, ":")
+	h = sha256.New()
+	h.Write([]byte(msg))
+
+	sig, err := keys["cloud"].Sign(rand.Reader, h.Sum(nil), crypto.SHA256)
+	if err != nil {
+		fmt.Println("signing msg:", err)
+	}
+	encodedSig := string(base64.StdEncoding.EncodeToString(sig))
+
+	req, _ := http.NewRequest(http.MethodPost, "https://api.apple-cloudkit.com"+path, bytes.NewBuffer(payload))
+	req.Header.Set("X-Apple-CloudKit-Request-KeyID", "b9f504ff7c0ef5d8b1dc6a1d12e597b3ab5fb9a8e6f24632486c15fb2a8d7f3e")
+	req.Header.Set("X-Apple-CloudKit-Request-ISO8601Date", date)
+	req.Header.Set("X-Apple-CloudKit-Request-SignatureV1", encodedSig)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("doing req:", err)
+	}
+
+	b, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(b))
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -112,7 +171,7 @@ func sign(w http.ResponseWriter, r *http.Request) {
 
 	bearer, err := jwtToken.SignedString(keys[svc])
 	if err != nil {
-		checkSyslog(logger.Err("signing bearer: " + err.Error()))
+		fmt.Println("signing bearer:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -130,5 +189,5 @@ func main() {
 
 	authEtc := etc + "auth"
 
-	checkSyslog(logger.Err(http.ListenAndServeTLS(":443", authEtc+"/server.crt", authEtc+"/server.key", r).Error()))
+	fmt.Println(http.ListenAndServeTLS(":443", authEtc+"/server.crt", authEtc+"/server.key", r))
 }
